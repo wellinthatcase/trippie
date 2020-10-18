da1754f3b18343f961632d5026c02f44958ae669
Py-Ryan <pyryanggg@gmail.com>
2020-10-18 18:52:39 -0400

    See eb384e9

--- dist/Client.js
+++ dist/Client.js
@@ -1,15 +1,23 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.TrippieClient = void 0;
+const pg_1 = require("pg");
 const discord_js_1 = require("discord.js");
 ;
 class TrippieClient extends discord_js_1.Client {
     constructor(config) {
         super();
         this.cfg = config;
+        const postgreUser = this.cfg.postgreUrlUser;
+        const postgrePass = this.cfg.postgreUrlPass;
+        const postgreDomain = this.cfg.postgreUrlDomain;
+        this.postgre = new pg_1.Client(`postgre://${postgreUser}:${postgrePass}@${postgreDomain}/${postgreUser}`);
+        this.postgre.connect().then(() => { console.log(`Connected into PostgreSQL DB "${postgreUser}"`); });
         this.login(this.cfg.token).then(() => {
-            console.log(`Successfully logged into ${this.user.username} (${this.user.id})`);
-        }, err => { console.error(err); });
+            console.log(`Logged into ${this.user.username} (${this.user.id})`);
+        }, err => {
+            console.error(err);
+        });
     }
 }
 exports.TrippieClient = TrippieClient;
