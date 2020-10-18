eb384e9f7834184a3caec4e916c91073a4f3b826
Py-Ryan <pyryanggg@gmail.com>
2020-10-18 18:47:17 -0400

    1. Update JSDoc comments on the TrippieClient class. 2. Implement PostgreSQL into the TrippieClient class. 3. Add PostgreSQL related properties into the TrippieCfg interface.
    Note, the database client construction and connection all happens
    inside the TrippieClient class constructor.

--- src/Client.ts
+++ src/Client.ts
@@ -1,3 +1,4 @@
+import { Client as PostgreClient } from "pg";
 import { Client as DiscordClient } from "discord.js"; 
 
 /**
@@ -6,24 +7,44 @@ import { Client as DiscordClient } from "discord.js";
  * @property token - The token of the TrippieClient bot. 
  * @property botId - The snowflake identification number of the bot. 
  * @property ownerId - The snowflake identification number of who owns the application. 
+ * @property postgreUrlUser - The username of the PostgreSQL database the bot must connect to. 
+ * @property postgreUrlPass - The password of the aforesaid PostgreSQL database. 
+ * @property postgreUrlDomain - The URL domain of the aforesaid PostgreSQL database. 
  */
 export interface TrippieCfg {
     readonly token: string;
     readonly botId: number; 
     readonly ownerId: number;
+    readonly postgreUrlUser: string, 
+    readonly postgreUrlPass: string, 
+    readonly postgreUrlDomain: string
 };
 
 /**
  * The essential class for the bot. 
+ * 
+ * @property cfg - The local config for the bot. 
+ * @property postgre - The PostgreSQL client for the bot. It is not connected until the bot has logged in. 
  */
 export class TrippieClient extends DiscordClient {
     protected cfg: TrippieCfg; 
+    protected postgre: PostgreClient; 
 
     constructor(config: TrippieCfg) {
         super();
         this.cfg = config; 
+
+        const postgreUser: string = this.cfg.postgreUrlUser;
+        const postgrePass: string = this.cfg.postgreUrlPass;
+        const postgreDomain: string = this.cfg.postgreUrlDomain; 
+        this.postgre = new PostgreClient(`postgre://${postgreUser}:${postgrePass}@${postgreDomain}/${postgreUser}`);
+
+        this.postgre.connect().then(() => { console.log(`Connected into PostgreSQL DB "${postgreUser}"`); });
+
         this.login(this.cfg.token).then(() => {
-            console.log(`Successfully logged into ${this.user.username} (${this.user.id})`);
-        }, err => { console.error(err); });
+            console.log(`Logged into ${this.user.username} (${this.user.id})`);
+        }, err => { 
+            console.error(err); 
+        });
     }
 }
