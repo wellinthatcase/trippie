48e089a4356c0edf691b469b59cdb78288b07de2
Py-Ryan <pyryanggg@gmail.com>
2020-10-17 23:31:50 -0400

    Login when constructed.

--- src/Client.ts
+++ src/Client.ts
@@ -1,16 +1,19 @@
 import * as discord from "discord.js"; 
 
-interface TrippieCfg {
+export interface TrippieCfg {
     token: string;
     bot_id: number; 
     owner_id: number;
 };
 
-class TrippieClient extends discord.Client {
+export class TrippieClient extends discord.Client {
     protected cfg: TrippieCfg; 
 
     constructor(config: TrippieCfg) {
         super();
         this.cfg = config; 
+        this.login(this.cfg.token).then(() => {
+            console.log(`Successfully logged in as ${this.user.username} (${this.user.id})`);
+        }, err => { console.error(err); });
     }
 }
