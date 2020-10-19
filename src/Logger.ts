57b715c18c5def7ccb1560d389cd64accdec6ac7
Py-Ryan <pyryanggg@gmail.com>
2020-10-18 23:21:53 -0400

    Add a cool colored logging feature.

---
+++ src/Logger.ts
@@ -0,0 +1,31 @@
+const red = "\x1b[31m";
+const green = "\x1b[32m";
+const yellow = "\x1b[33m";
+const resetter = "\x1b[0m";
+
+export const enum LogLevel {
+    Success,
+    Warning,
+    Exception
+};
+
+/**
+ * Log to the standard output with one of the optional colors. 
+ * 
+ * @param text - The text to output. 
+ * @param log_level - The LogLevel enum selection. Success is green, Warning yellow, and Exception red. 
+ */
+export function log(text: string, log_level?: LogLevel | undefined): void {
+    const text_with_resetter = `${text}${resetter}`; 
+
+    switch (log_level) {
+        case LogLevel.Exception:
+            console.log(`${red}${text_with_resetter}`);
+            break;
+        case LogLevel.Warning: 
+            console.log(`${yellow}${text_with_resetter}`);
+            break;
+        case LogLevel.Success || undefined: 
+            console.log(`${green}${text_with_resetter}`);
+    }; 
+}; 
