fea0611549e3ba5622643704a31467881835a683
Py-Ryan <pyryanggg@gmail.com>
2020-10-19 01:01:35 -0400

    See b520932

--- dist/Logger.js
+++ dist/Logger.js
@@ -7,9 +7,9 @@ const yellow = "\x1b[33m";
 const resetter = "\x1b[0m";
 ;
 function log(text, log_level) {
-    const timestamp = new Date()
-        .toLocaleDateString("en-US", { hour: "numeric", minute: "numeric", hour12: true });
     const content = `[${timestamp}]: ${text}${resetter}`;
+    var timestamp = new Date()
+        .toLocaleDateString("en-US", { hour: "numeric", minute: "numeric", hour12: true });
     switch (log_level) {
         case 2:
             console.log(`${red}${content}`);
