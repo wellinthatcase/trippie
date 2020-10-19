349d9405930d38483c8656221cb57c49659d58e4
Py-Ryan <pyryanggg@gmail.com>
2020-10-19 14:56:57 -0400

    See ab36fb5

--- dist/Logger.js
+++ dist/Logger.js
@@ -1,24 +1,44 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.log = void 0;
-const red = "\x1b[31m";
-const green = "\x1b[32m";
-const yellow = "\x1b[33m";
-const resetter = "\x1b[0m";
+exports.Logger = void 0;
 ;
-function log(text, log_level) {
-    const timestamp = new Date()
-        .toLocaleDateString("en-US", { hour: "numeric", minute: "numeric", hour12: true });
-    const content = `[${timestamp}]: ${text}${resetter}`;
-    switch (log_level) {
-        case 2:
-            console.log(`${red}${content}`);
-        case 1:
-            console.log(`${yellow}${content}`);
-        case 0 || undefined:
-            console.log(`${green}${content}`);
+;
+class Logger {
+    constructor(cfg) {
+        this.cfg = {
+            "alwaysResetter": cfg.alwaysResetter || true,
+            "defaultResetter": cfg.defaultResetter || "\x1b[0m",
+            "defaultSuccessFg": cfg.defaultSuccessFg || "\x1b[32m",
+            "defaultWarningFg": cfg.defaultWarningFg || "\x1b[33m",
+            "defaultExceptiFg": cfg.defaultExceptiFg || "\x1b[31m"
+        };
+    }
+    ;
+    log(text, log_level) {
+        if (this.cfg.alwaysResetter) {
+            var resetter = this.cfg.defaultResetter;
+        }
+        else {
+            var resetter = "";
+        }
+        ;
+        const timestamp = new Date()
+            .toLocaleDateString("en-US", { hour: "numeric", minute: "numeric", hour12: true });
+        const warning = this.cfg.defaultWarningFg;
+        const success = this.cfg.defaultSuccessFg;
+        const excepti = this.cfg.defaultExceptiFg;
+        const content = `[${timestamp}]: ${text}${resetter}`;
+        switch (log_level) {
+            case 2:
+                console.log(`${excepti}${content}`);
+            case 1:
+                console.log(`${warning}${content}`);
+            case 0 || undefined:
+                console.log(`${success}${content}`);
+        }
+        ;
     }
     ;
 }
-exports.log = log;
+exports.Logger = Logger;
 ;
