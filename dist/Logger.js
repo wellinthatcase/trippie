aba30bfd70b157d34d70315e68cddf1f8fb3cdb7
Py-Ryan <pyryanggg@gmail.com>
2020-10-20 00:59:47 -0400

    See 0b2330d

--- dist/Logger.js
+++ dist/Logger.js
@@ -1,11 +1,33 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Logger = void 0;
+const fs = __importStar(require("fs"));
 ;
 ;
 class Logger {
     constructor(cfg) {
         this.cfg = {
+            "logFile": cfg.logFile || null,
+            "alwaysLog": cfg.alwaysLog || false,
             "alwaysResetter": cfg.alwaysResetter || true,
             "defaultResetter": cfg.defaultResetter || "\x1b[0m",
             "defaultSuccessFg": cfg.defaultSuccessFg || "\x1b[32m",
@@ -14,7 +36,7 @@ class Logger {
         };
     }
     ;
-    log(text, log_level) {
+    log(text, log_level, write) {
         if (this.cfg.alwaysResetter) {
             var resetter = this.cfg.defaultResetter;
         }
@@ -27,14 +49,24 @@ class Logger {
         const warning = this.cfg.defaultWarningFg;
         const success = this.cfg.defaultSuccessFg;
         const excepti = this.cfg.defaultExceptiFg;
-        const content = `[${timestamp}]: ${text}${resetter}`;
+        const content = `[${timestamp}]: ${text}`;
+        const resetterContent = `${content}${resetter}`;
         switch (log_level) {
             case 2:
-                console.log(`${excepti}${content}`);
+                console.log(`${excepti}${resetterContent}`);
             case 1:
-                console.log(`${warning}${content}`);
+                console.log(`${warning}${resetterContent}`);
             case 0 || undefined:
-                console.log(`${success}${content}`);
+                console.log(`${success}${resetterContent}`);
+        }
+        ;
+        if (write === true || (write === undefined && this.cfg.alwaysLog)) {
+            fs.appendFile(this.cfg.logFile, `${content}\n`, (err) => {
+                if (err) {
+                    console.error(err);
+                }
+                ;
+            });
         }
         ;
     }
