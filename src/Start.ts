81aae25a5b2e2b63a562d48ad21702cfac00bf45
Py-Ryan <pyryanggg@gmail.com>
2020-10-22 03:53:09 -0400

    Add baseplate context construction for future commands.

--- src/Start.ts
+++ src/Start.ts
@@ -1,4 +1,24 @@
 import cfg from "./cfg/cfg.json";
+import { Context } from "./Context";
 import { TrippieClient, TrippieCfg } from "./Client"; 
 
 const client: TrippieClient = new TrippieClient(cfg as TrippieCfg); 
+
+client.on("message", (message) => {
+    if (message.author === client.user) return;
+
+    const con: string   = message.content;
+    const pre: string   = con.charAt(0); 
+    const arg: string[] = con.split(" ");
+    const cmd: string   = con.substring(0, arg[0].length - 1); 
+
+    const context: Context = new Context(
+        cmd,
+        pre,
+        arg,
+        client,
+        message
+    );
+
+    context.send(con, { embed: true }); 
+});
