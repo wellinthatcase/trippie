127ae933657ac28153f90714f2dc414ecb7326ae
Py-Ryan <pyryanggg@gmail.com>
2020-10-22 03:55:26 -0400

    See 5403402

--- dist/Logger.js
+++ dist/Logger.js
@@ -1,26 +1,7 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Logger = void 0;
-const fs = __importStar(require("fs"));
+const fs_1 = require("fs");
 ;
 ;
 class Logger {
@@ -61,7 +42,7 @@ class Logger {
         }
         ;
         if (write === true || (write === undefined && this.cfg.alwaysLog)) {
-            fs.appendFile(this.cfg.logFile, `${content}\n`, (err) => {
+            fs_1.appendFile(this.cfg.logFile, `${content}\n`, (err) => {
                 if (err) {
                     throw err;
                 }
