12566dedd72a16fbdf338198296b1a9c67e02a85
Py-Ryan <pyryanggg@gmail.com>
2020-11-05 04:33:20 -0500

    Use new infant command handler.

--- src/Start.ts
+++ src/Start.ts
@@ -2,13 +2,15 @@ import cfg from "./cfg/cfg.json";
 import { Context } from "./Context";
 import { Message } from "discord.js"; 
 import { TrippieClient } from "./Client"; 
+import { CommandHandler } from "./Commands"; 
 
 const client: TrippieClient = new TrippieClient(cfg); 
+const commands: CommandHandler = new CommandHandler("./src/Commands");
 
 client.on("message", async (msg) => {
     if (msg.author === client.user || !msg.guild || !msg.content.startsWith(">")) 
         return;
 
     const context: Context = new Context(msg, ">", client);
-    const message: Message = await context.send(context.msg.content); 
+    commands.processMessage(context);
 });
